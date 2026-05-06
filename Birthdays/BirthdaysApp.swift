93aaa130c666ef349b24ca3e2ba246c2684f74b4
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-05-06 13:41:53 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Lidiia Diachkovskaia on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
